876ed85622eedac7d20e5fc5f40adbe1029ea747
Carlos Peralta <carlos9917@gmail.com>
2023-03-09 12:26:34 +0100

    first commit

---
+++ all_experiment_names.R
@@ -0,0 +1,4 @@
+# List of experiment names to consider in the visualisation app
+# Note: This list should follow the convention: "label" = "relevant directory/string in filename"
+# New labels should be added as appropriate for new options
+all_exp_names <- list("sample models" = "model")
